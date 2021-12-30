7aa3963ae65c7f4dabdaf13ae1596faf68e60655
Jaewook <eari100@naver.com>
2021-12-30 22:49:36 +0900

    Initial commit

---
+++ src/main/java/com/example/studyspringboottest/StudySpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.studyspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudySpringBootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudySpringBootTestApplication.class, args);
+    }
+
+}
