4860202482761dfebd2a3e9945842b476a89068c
Jaewook <eari100@naver.com>
2021-12-31 22:39:36 +0900

    feat: "/books" 호출 API 추가

---
+++ src/main/java/com/example/studyspringboottest/service/BookService.java
@@ -0,0 +1,9 @@
+package com.example.studyspringboottest.service;
+
+import com.example.studyspringboottest.domain.Book;
+
+import java.util.List;
+
+public interface BookService {
+    List<Book> getBookList();
+}
